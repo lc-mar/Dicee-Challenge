860d1bfb76c0a3a7e6185f1f6a7ae7c667f82fc1
Lauren <78833615+lc-mar@users.noreply.github.com>
2022-11-08 15:39:31 -0500

    added randomDice var

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; // gives a random # between 1-6 
 
-
+var randomDice = "dice" + randomNumber1 + ".png"; // retrieves random dice image from folder
