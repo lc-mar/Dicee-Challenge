1a884a24d8a71a3d63fae7e8d2487dc477b0a67f
Lauren <78833615+lc-mar@users.noreply.github.com>
2022-11-11 15:57:34 -0500

    updating js and updated text size in css style

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ image1.setAttribute("src", randomImageSource);
 var randomNumber2 = Math.floor(Math.random() * 6) + 1; // gives a random # between 1-6 for dice #2 
 
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; // retrieves random dice image from folder to attach # in random # on dice 2
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🎉 Player 1 Wins! 🎉"
+}
+
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "🎉 Player 2 Wins 🎉"
+}
