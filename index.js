31f4aeb460e721de53615411c344e6290e89daa6
Lauren <78833615+lc-mar@users.noreply.github.com>
2022-11-11 15:52:01 -0500

    updating js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; // gives a random # between 1-6 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // gives a random # between 1-6 for dice # 1
 
-var randomDice = "dice" + randomNumber1 + ".png"; // retrieves random dice image from folder
+var randomDice = "dice" + randomNumber1 + ".png"; // retrieves random dice image from folder to attach # in random # on dice 1 
 
 
 var randomImageSource = "images/" + randomDice; //changes dice images
@@ -8,3 +8,7 @@ var randomImageSource = "images/" + randomDice; //changes dice images
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // gives a random # between 1-6 for dice #2 
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; // retrieves random dice image from folder to attach # in random # on dice 2
